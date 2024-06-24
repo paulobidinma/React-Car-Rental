e330b338d90249373db20fd3fcae2737ffbae45a
ozzyofficial <oziomaobidinma3@gmail.com>
2024-06-24 22:47:35 +0100

    Update

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -43,7 +43,7 @@ const Header = () => {
               <div className="header__top__left">
                 <span>Need Help?</span>
                 <span className="header__top__help">
-                  <i class="ri-phone-fill"></i> +1-202-555-0149
+                  <i class="ri-phone-fill"></i> +234phonenumber
                 </span>
               </div>
             </Col>
@@ -86,8 +86,8 @@ const Header = () => {
                   <i class="ri-earth-line"></i>
                 </span>
                 <div className="header__location-content">
-                  <h4>Bangladesh</h4>
-                  <h6>Sylhet City, Bangladesh</h6>
+                  <h4>Any Country</h4>
+                  <h6>Any City, Any Country</h6>
                 </div>
               </div>
             </Col>
@@ -98,8 +98,8 @@ const Header = () => {
                   <i class="ri-time-line"></i>
                 </span>
                 <div className="header__location-content">
-                  <h4>Sunday to Friday</h4>
-                  <h6>10am - 7pm</h6>
+                  <h4>Monday to Friday</h4>
+                  <h6>9am - 5pm</h6>
                 </div>
               </div>
             </Col>
